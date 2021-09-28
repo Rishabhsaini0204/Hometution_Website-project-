0f6b03629c9409fbf12d4acfc5e6c20eb0a58a75
Rishabh Saini <78493976+Rishabhsaini0204@users.noreply.github.com>
2021-09-28 13:13:30 +0530

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,106 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "rishabhsaini0204@gmail.com";
+    $email_subject = "Message from Hometution.com";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['phone']) ||
+        !isset($_POST['subject']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $phone = $_POST['phone']; // required
+    $subject = $_POST['subject']; // required
+    $message = $_POST['message']; // required
+    
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+  }
+ 
+   
+  if(strlen($message) < 1) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Phone: ".clean_string($phone)."\n";
+    $email_message .= "Subject: ".clean_string($subject)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+
+?>
+<!-- Html file here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Tuition|Thank you</title>
+    <link rel="stylesheet" href="thankstyle.css">
+</head>
+<body>
+    <section class="thank-main-wrapper">
+                    <div class="thankyou-wrapper">
+                        <h1><img src="http://montco.happeningmag.com/wp-content/uploads/2014/11/thankyou.png" alt="thanks" /></h1>
+                        <p id="snd-contact">Thanks a bunch for filling that out. It means a lot to us, just like you do! <br>We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
+                        <p>we will get in touch with you soon</p>
+                        <a href="index.html">Back to home</a>
+                    
+      </section>
+</body>
+</html>
+
+<?php
+ 
+}
+?>
